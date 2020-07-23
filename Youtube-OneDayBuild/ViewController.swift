83289b2a9b7da133443657c86fc830fc4813c877
Linh Tuan <linhbun2@gmail.com>
2020-07-23 09:53:54 +0700

    initial xcode project creation

---
+++ Youtube-OneDayBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube-OneDayBuild
+//
+//  Created by Linh Tuan on 7/23/20.
+//  Copyright © 2020 Linh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
